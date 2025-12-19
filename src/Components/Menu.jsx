481d910e5acd665191a539c113895b787ee93292
Juan León <depechejuan@gmail.com>
2025-12-19 17:37:53 +0100

    arbol y estructura basica creada

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,5 @@
+function Menu() {
+    return <nav>Test</nav>
+}
+
+export default Menu;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+
+    return (
+        <h1>Gestaurante</h1>
+    )
+}
+
+export default Home;
